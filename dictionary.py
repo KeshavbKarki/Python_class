cf77f35a1f3e92655c25cff30501e21361d4e6b3
keshavbkarki <keshav.bdrkarki@gmail.com>
2025-12-12 21:34:28 +0545

    sets

---
+++ dictionary.py
@@ -0,0 +1,29 @@
+car = {
+    
+    "brand" : "toyota",
+    "color" : ["black","white","red"],
+    "model" : 2025,
+    "price" : 2000000
+    
+}
+# print(car)  
+   
+#accessing the value from dictionary
+
+# a = car["brand"]
+# b = car["color"]
+# a = car.get("brand")
+# b = car.get("price")
+# print(a)
+# print(b)
+
+
+# a = car.keys()
+# print(a)
+# b = car.values()
+# print(b)
+
+# Changing values of dictionary
+# car["model"] = 2027
+# car["price"] = 5000000
+# print(car)
---
+++ tuples.py
@@ -0,0 +1,13 @@
+
+
+"""
+Lists vs Tuples:
+List: 
+ordered, changeable, allows duplicate members., syntax [], methods are available, accessing the value is slow, data are not secured as compared to tuples
+Tuple:
+ordered, unchangeable, allows duplicate members. syntax (), methods are not available, accessing the value is fast, data are secured here than list
+Set:
+unordered, unchangeable, doesn't allow duplicate members, syntax {}, methods are available only for adding or removing data, 
+Dictionary: 
+ordered/unordered , changeable, doesn't allow duplicate members,  syntax = {}, methods are available 
+"""
